26abac9434326af07ab4a02f2038ea98cd46c509
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-20 11:13:17 +0530

    Importing the Functions as Themselves and Using them by their Name

--- Hello.js
+++ Hello.js
@@ -1,8 +1,12 @@
 // console.log("Hey there! I am JS");
-const math=require('./math.js');
+// const math=require('./math.js');
+const {add, sub} =require('./math.js');
 
-console.log(math.add(2, 5));
-console.log(math.sub(2, 5));
+// console.log(math.add(2, 5));
+// console.log(math.sub(2, 5));
+ 
+console.log(add(2, 5));
+console.log(sub(2, 5));
 
 
 
