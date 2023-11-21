0dbbd9d135e3f7da60e28c70223fba52b57181f2
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-21 17:24:31 +0530

    Creating Server Using ExpressJS

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,23 +1,27 @@
-const express= require("express");
-const http=require("http");
+const express = require("express");
+// const http=require("http");
 
-const app=express();
+const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     return res.send("Hello from Home Page");
 });
 
-app.get('/about', (req, res)=>{
-    return res.send("Hello from About Page");
+app.get('/about', (req, res) => {
+    return res.send("Hello from About Page" + `, Hello ${req.query.name}`);
 });
 
-app.get('/about/feed', (req, res)=>{
+app.get('/about/feed', (req, res) => {
     return res.send("Hello from Feed Page");
 })
 
-const myServer=http.createServer(app);
-
-myServer.listen(3000, ()=>{
+app.listen(3000, (req, res) => {
     console.log("Server Started");
-});
+})
+
+// const myServer=http.createServer(app);
+
+// myServer.listen(3000, ()=>{
+//     console.log("Server Started");
+// });
 
