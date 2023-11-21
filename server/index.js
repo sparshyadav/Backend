140100aa44ccde12b52284cb2a38edac22f6ae43
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-21 09:04:44 +0530

    Handling Queries in the URL

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+        return res.end();
+    }
     const log = `${Date.now()} : ${req.url} New Request Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/': res.end("Homepage")
                 break;
-            case '/about': res.end("About Page");
+            case '/about': 
+                const qp = myUrl.query.name;
+                res.end(`Hi, ${qp}`);
                 break;
             default: res.end("ERROR 404 Page Not Found");
         }
