775ef723d3885b20b2e9eabba3a3a44f3e0b5be4
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-21 10:27:11 +0530

    Updating GET POST Requests in Log File

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const myServer = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") {
         return res.end();
     }
-    const log = `${Date.now()} : ${req.url} New Request Received\n`;
+    const log = `${Date.now().toLocalString()} : ${req.url} : ${req.method} New Request Received\n`;
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
