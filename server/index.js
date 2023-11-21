9643b9821ba72c481db78589e92191d2e4a18fed
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-21 08:33:11 +0530

    Creating a Log File

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-const http=require("http");
+const http = require("http");
+const fs = require("fs");
 
-const myServer=http.createServer((req, res)=>{
-    console.log("New Requence Received");
-    res.end("Hello From Server");
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: New Request Received\n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+        res.end("Hello From Server");
+    })
 });
 
-myServer.listen(8000, ()=>console.log("Server Started"));
+myServer.listen(3000, () => console.log("Server Started"));
