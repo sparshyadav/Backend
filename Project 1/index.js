7d25f63cd77bfc099631beaa8eb522a0f9a4e54b
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-22 09:14:24 +0530

    Displaying Any Searched User Information

--- Project 1/index.js
+++ Project 1/index.js
@@ -24,10 +24,15 @@ app.get('/users', (req, res) => {
     return res.send(html);
 });
 
+//REST API 
 app.get('/api/users', (req, res) => {
     return res.json(users);
 });
 
-
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
