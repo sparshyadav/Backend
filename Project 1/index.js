68d3ffc74d83671464abe52babb942d720105f55
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-22 09:00:41 +0530

    Server Side Rendering - Displaying All the Users First Name

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,18 +1,32 @@
-const express=require("express");
-const users=require("./MOCK_DATA.json");
+const express = require("express");
+const users = require("./MOCK_DATA.json");
 
-const app=express();
-const PORT=8000;
+const app = express();
+const PORT = 8000;
 
 //Routes
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     return res.send("Welcome to Home Page");
 });
 
-app.get('/users', (req, res)=>{
+app.get('/users', (req, res) => {
+    const html =
+        `
+    <ul>
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        ${users.map((user) => `<li>${user.last_name}</li>`).join("")}
+        ${users.map((user) => `<li>${user.email}</li>`).join("")}
+        ${users.map((user) => `<li>${user.gender}</li>`).join("")}
+        ${users.map((user) => `<li>${user.job_title}</li>`).join("")}
+    </ul>
+    `
+    return res.send(html);
+});
+
+app.get('/api/users', (req, res) => {
     return res.json(users);
 });
 
 
 
-app.listen(PORT, ()=> console.log(`Server Started at PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
