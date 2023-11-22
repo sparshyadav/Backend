2ec323e55787a2b5effae0c531c5f6aa37920320
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-22 08:49:31 +0530

    GET Request created for task 1 - ALL USERS

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,8 +1,18 @@
 const express=require("express");
+const users=require("./MOCK_DATA.json");
 
 const app=express();
 const PORT=8000;
 
 //Routes
+app.get('/', (req, res)=>{
+    return res.send("Welcome to Home Page");
+});
+
+app.get('/users', (req, res)=>{
+    return res.json(users);
+});
+
+
 
 app.listen(PORT, ()=> console.log(`Server Started at PORT: ${PORT}`));
